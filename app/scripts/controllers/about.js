2573ecbb0c4d85b3809f4bb4fc1923384da3fa4f
Shin <mrmaxin@gmail.com>
2016-07-20 15:53:31 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTestDeploymentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTestDeploymentApp
+ */
+angular.module('angularTestDeploymentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTestDeploymentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTestDeploymentApp
+ */
+angular.module('angularTestDeploymentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
