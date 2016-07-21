7c80addbfa3e0a60a16d13c7acf9c79c79d71015
Shin <mrmaxin@gmail.com>
2016-07-20 21:07:19 -0700

    adjust grunt.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,9 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-
+  grunt.loadNpmTasks('grunt-build-control');
   var pkg = require('./package.json');
+  
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -445,7 +446,7 @@ module.exports = function (grunt) {
         }
       }
     },
-    
+
     // Test settings
     karma: {
       unit: {
