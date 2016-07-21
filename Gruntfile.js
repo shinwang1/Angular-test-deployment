80ff09754d49de7adb2ebe91e0acd518db90f36b
Shin <mrmaxin@gmail.com>
2016-07-20 20:51:33 -0700

    add grunt build control

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@
 
 module.exports = function (grunt) {
 
+  var pkg = require('./package.json');
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
@@ -417,6 +418,34 @@ module.exports = function (grunt) {
       ]
     },
 
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:git@github.com:shinwang1/Angular-test-deployment.git',
+          branch: 'gh-pages'
+        }
+      },
+      heroku: {
+        options: {
+          remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+          branch: 'master',
+          tag: pkg.version
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
+    },
+    
     // Test settings
     karma: {
       unit: {
