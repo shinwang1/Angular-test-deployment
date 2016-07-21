35e02ec43db31beb2b4e6fe5db4ef83823ba6593
Shin <mrmaxin@gmail.com>
2016-07-20 21:13:27 -0700

    add heroku remote endpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -434,7 +434,7 @@ module.exports = function (grunt) {
       },
       heroku: {
         options: {
-          remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+          remote: 'git@heroku.com:https://git.heroku.com/glacial-bayou-81897.git',
           branch: 'master',
           tag: pkg.version
         }
